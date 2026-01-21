1e54390295b245dbbfeede175af66845ed426222
katyazano <156813907+katyazano@users.noreply.github.com>
2026-01-20 20:00:02 -0600

    Add files via upload

---
+++ katheryn-tema1/katheryn-act1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  katheryn-act1
+//
+//  Created by Alumno on 20/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ katheryn-tema1/katheryn-act1Tests/katheryn_act1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  katheryn_act1Tests.swift
+//  katheryn-act1Tests
+//
+//  Created by Alumno on 20/01/26.
+//
+
+import Testing
+@testable import katheryn_act1
+
+struct katheryn_act1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
